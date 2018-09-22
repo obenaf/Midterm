9ae2b28168d58fa29b8785c4ab6ce6bc19dc8811
obenaf <obenaf96@gmail.com>
2018-09-21 17:37:16 -0700

    Will crash your machine don't fucking touch this distro rn

--- CS210_Midterm_VW3.cpp
+++ CS210_Midterm_VW3.cpp
@@ -12,56 +12,111 @@
 #include<string>
 #include<fstream>
 using namespace std;
+//Keyword array
+string Keywords[37] = {"accessor", "and", "array", "begin", "bool",
+                       "case", "character", "constant", "else", "elsif",
+                       "end", "exit", "function", "if", "in", "integer",
+                       "interface", "is", "loop", "module", "mutator",
+                       "natural", "null", "of", "or", "others", "out",
+                       "positive", "procedure", "range", "return", "struct",
+                       "subtype", "then", "type", "when", "while"};
+//Operators array
+string Operators[27] = {".", "<", ">", "(", ")", "+", "-", "*", "/",
+                        "|", "&", ";", ",", ":", "[", "]", "=", ":=",
+                        "..", "<<", ">>", "<>", "<=", ">=", "**",
+                        "!=", "=>"};
+//Token class array
+string tokClass[8] = {"comment", "operator", "string", "keyword", "character literal",
+                      "numeric literal", "identifier", "UNK"};
 //Global Variables
-int charClass;
 char activeChar;
+string sLexeme;
 char lexeme[63];
+char nextChar;
+int charClass;
 int lexLength;
+string token;
+int nextToken;
+FILE *primeRead;
+FILE *primeWrite;
 //Function Headers
+bool strCompare();
 void analyze();
-void readChar(ifstream&);
+void readChar();
 void lexAdd();
-void traverseWhiteSpace();
+void traverseSpace();
+void unknown();
+//Classes
+#define ALPHA 0
+#define DIGIT 1
+#define UNK -1
 
 int main(int argc, char **argv) {
     //sets I/O file names
-    string primeInput = argv[1];
-    string primeOutput = primeInput + ".cpy";
-
+    string readFile = argv[1];
+    string writeFile = readFile + ".unk";
+    ifstream primeRead;
+    ofstream primeWrite;
     //opens I/O files
-    ifstream primeRead(primeInput);
-    ofstream primeWrite(primeOutput);
+    primeRead.open(readFile);
+    primeWrite.open(writeFile);
 
-    while(!primeRead.eof()) { //Writes directly to out file for now
-        primeRead.get(activeChar);
-        primeWrite << activeChar;
-    }
-    return 0; //TERMINATES PROGRAM
-    //Program not ready for implementation
-    readChar(primeRead);
+    readChar();
     do {
         analyze();
-    } while (activeChar != EOF);
+        primeWrite << lexeme << " (" << token << ")\n";
+    } while (nextToken != EOF);
     return 0;
 }
 //Sorts characters into subgroups
 void analyze() {
-    lexLength = 0;
-    traverseWhiteSpace();
-    switch(charClass) { //unimplemented
+    lexLength = 0; //resets lexLength
+    traverseSpace();
+    switch(charClass) {
+        case ALPHA:
+            lexAdd();
+            readChar();
+            //As long as the chars meet criteria for keywords/identifiers
+            while(charClass == ALPHA ||charClass == DIGIT) {
+                lexAdd();
+                readChar();
+            }
+            sLexeme = lexeme;
+                if(strCompare())//if lexeme matches keyword list
+                    token = tokClass[3]; //sets as keyword
+                    else //not a keyword
+                    token = tokClass[6]; //sets as identifier
+                    break;
+        case UNK:
+            if(activeChar == '/' && nextChar == '*') { //if lexeme is comment
+                do {
+                    lexAdd();
+                    readChar();
+                } while (activeChar != '*' && nextChar != '/');
+            }
+                else if(activeChar == '"') { //if lexeme is string
+                    do {
+                        lexAdd();
+                        readChar();
+                    } while(activeChar != '"');
+                }
+                //else if(activeChar ==  NONFUNCTIONAL
+                else
+                    unknown();
+                  break;
+            }
 
     }
-}
 //Reads characters and determines char type
-void readChar(ifstream &primeRead) {
-    primeRead.get(activeChar);
-    if(activeChar != EOF) {
+void readChar() {
+    activeChar = nextChar;
+    if((nextChar = getc(primeRead)) != EOF) {
     if(isalpha(activeChar))
-        charClass = -1;
+        charClass = ALPHA;
     else if(isdigit(activeChar))
-        charClass = 0;
+        charClass = DIGIT;
     else
-        charClass = 1;
+        charClass = UNK;
     }
     else charClass = EOF;
 }
@@ -71,23 +126,20 @@ void lexAdd() {
     lexeme[lexLength] = 0;
 }
 //finds the next character
-void traverseWhiteSpace() {
-
+void traverseSpace() {
+    while(isspace(nextChar))
+        getchar();
 }
 
-//Keyword array
-string Keywords[37] = {"accessor", "and", "array", "begin", "bool",
-              "case", "character", "constant", "else", "elsif",
-              "end", "exit", "function", "if", "in", "integer",
-              "interface", "is", "loop", "module", "mutator",
-              "natural", "null", "of", "or", "others", "out",
-              "positive", "procedure", "range", "return", "struct",
-              "subtype", "then", "type", "when", "while"};
+bool strCompare() {
+    for(int i = 0; i < 37; i++) {
+        if(sLexeme == Keywords[i])
+            return true;
+    }
+    return false;
+}
 
-string Operators[27] = {".", "<", ">", "(", ")", "+", "-", "*", "/",
-                        "|", "&", ";", ",", ":", "[", "]", "=", ":=",
-                        "..", "<<", ">>", "<>", "<=", ">=", "**",
-                        "!=", "=>"};
-
-string lexClass[8] = {"comment", "operator", "string", "keyword", "character literal",
-                      "operator", "numeric literal", "UNK"};
+void unknown() {
+    sLexeme = activeChar;
+    token = "UNK";
+}
