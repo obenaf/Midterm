464e23e7cd7827364add5e8945811a770dad368b
obenaf <obenaf96@gmail.com>
2018-09-24 18:48:12 -0700

    File systems, how do they work?

--- CS210_Midterm_VW3.cpp
+++ CS210_Midterm_VW3.cpp
@@ -2,12 +2,11 @@
  * Built by Richard Deming
  * github.com/obenaf
  * Midterm for CS-210
- * 18 Sep 2018
+ * 24 Sep 2018
  */
  //NOTE: I haven't decided how I want to write to the output file
  //yet, will implement that when I am further along
-
-#include<cstdio>
+#include<stdio.h>
 #include<iostream>
 #include<string>
 #include<fstream>
@@ -30,11 +29,11 @@ string tokClass[8] = {"comment", "operator", "string", "keyword", "character lit
                       "numeric literal", "identifier", "UNK"};
 //Global Variables
 char activeChar;
-string sLexeme;
-char lexeme[63];
 char nextChar;
+string sLexeme;
+char lexeme[64];
 int charClass;
-int lexLength = 0;
+int lexLength;
 string token;
 int nextToken;
 FILE *primeRead;
@@ -54,75 +53,87 @@ int main(int argc, char **argv) {
     //sets I/O file names
     string readFile = argv[1];
     string writeFile = readFile + ".unk";
-    ifstream primeRead;
-    ofstream primeWrite;
     //opens I/O files
-    primeRead.open(readFile);
-    primeWrite.open(writeFile);
+    ifstream primeRead(readFile, ios::in);
+    ofstream primeWrite(writeFile, ios::out);
 
+    primeRead.get(nextChar);
     readChar();
-    do {
+    while (nextToken != EOF) {
         analyze();
         primeWrite << lexeme << " (" << token << ")\n";
-    } while (nextToken != EOF);
+        traverseSpace();
+        readChar();
+    }
+    primeRead.close();
+    primeWrite.close();
     return 0;
+}
+//Reads characters and determines char type
+void readChar() {
+    activeChar = nextChar;
+    if((nextChar = getc(primeRead)) != EOF) {
+        if(isalpha(activeChar))
+            charClass = ALPHA;
+        else if(isdigit(activeChar))
+            charClass = DIGIT;
+        else
+            charClass = UNK;
+    }
+    else charClass = EOF;
 }
 //Sorts characters into subgroups
 void analyze() {
     lexLength = 0; //resets lexLength
     switch(charClass) {
-        case ALPHA:
-            lexAdd();
-            readChar();
+        case DIGIT:
+            while(nextChar != ' ') {
+                lexAdd();
+                readChar();
+            }
+            sLexeme = activeChar;
+            token = "UNK";
+            break;
+        case ALPHA: //Case for keywords and identifiers
             //As long as the chars meet criteria for keywords/identifiers
-            while(charClass == ALPHA ||charClass == DIGIT) {
+            while (charClass == ALPHA || charClass == DIGIT) {
                 lexAdd();
                 readChar();
             }
             sLexeme = lexeme;
-                if(strCompare())//if lexeme matches keyword list
-                    token = tokClass[3]; //sets as keyword
-                    else //not a keyword
-                    token = tokClass[6]; //sets as identifier
-                    break;
+            if (strCompare())//if lexeme matches keyword list
+                token = tokClass[3]; //sets as keyword
+            else //not a keyword
+                token = tokClass[6]; //sets as identifier
+            break;
         case UNK:
-            if(activeChar == '/' && nextChar == '*') { //if lexeme is comment
+            if (activeChar == '/' && nextChar == '*') { //if lexeme is comment
                 do {
                     lexAdd();
                     readChar();
                 } while (activeChar != '*' && nextChar != '/');
+            } else if (activeChar == '"') { //if lexeme is string
+                do {
+                    lexAdd();
+                    readChar();
+                } while (activeChar != '"');
             }
-                else if(activeChar == '"') { //if lexeme is string
-                    do {
-                        lexAdd();
-                        readChar();
-                    } while(activeChar != '"');
-                }
                 //else if(activeChar ==  NONFUNCTIONAL
-                else
-                    unknown();
-                  break;
+            else
+                while(nextChar != ' ') {
+                    lexAdd();
+                    readChar();
+                }
+            sLexeme = activeChar;
+            token = "UNK";
+            break;
         case EOF:
             nextToken = EOF;
             break;
-            }
-
+    }
 
 
     }
-//Reads characters and determines char type
-void readChar() {
-    activeChar = nextChar;
-    if((nextChar = getc(primeRead)) != EOF) {
-    if(isalpha(activeChar))
-        charClass = ALPHA;
-    else if(isdigit(activeChar))
-        charClass = DIGIT;
-    else
-        charClass = UNK;
-    }
-    else charClass = EOF;
-}
 //adds activeChar into the working lexeme
 void lexAdd() {
     lexeme[lexLength++] = activeChar;
@@ -130,8 +141,8 @@ void lexAdd() {
 }
 //finds the next character
 void traverseSpace() {
-    while(isspace(nextChar))
-        nextChar = getc(primeRead);
+
+primeRead get(nextChar);
 }
 
 bool strCompare() {
@@ -142,7 +153,3 @@ bool strCompare() {
     return false;
 }
 
-void unknown() {
-    sLexeme = activeChar;
-    token = "UNK";
-}
